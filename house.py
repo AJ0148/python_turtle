b71fc439573a81c7f71f5107c5358dbd7e699e18
AJ0148 <155866630+AJ0148@users.noreply.github.com>
2024-07-17 10:28:15 -0400

    built rooms in house

--- house.py
+++ house.py
@@ -8,4 +8,23 @@ while True:
     if abs(t.pos()) < 1:
         break
 
+t.penup()
+t.fd(50)
+t.pendown()
+t.left(90)
+t.fd(50)
+t.left(90)
+t.fd(50)
+t.penup()
+t.left(180)
+t.fd(50)
+t.pendown()
+t.fd(50)
+t.penup()
+t.left(180)
+t.fd(50)
+t.right(90)
+t.pendown()
+t.fd(50)
+
 t.exitonclick()
