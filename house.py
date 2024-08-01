5cc186184896ce250c77461046839500c932bf28
AJ0148 <155866630+AJ0148@users.noreply.github.com>
2024-08-01 19:32:41 -0400

    build 2nd house

--- house.py
+++ house.py
@@ -29,6 +29,37 @@ t.fd(70)
 t.left(135)
 t.fd(100)
 t.end_fill()
+t.penup()
+
+t.goto(175, 0)
+t.pencolor("green")
+t.pendown()
+
+for i in range(4):
+    t.fd(100)
+    t.left(90)
+
+for i in range(4):
+    t.penup()
+    t.goto(225, 50)
+    t.pendown()
+    t.left(90)
+    t.fd(50)
+
+t.goto(275, 100)
+t.right(225)
+t.pencolor("black")
+t.fillcolor("black")
+t.pendown()
+t.begin_fill()
+t.fd(70)
+t.left(90)
+t.fd(70)
+t.left(135)
+t.fd(100)
+t.end_fill()
+t.penup()
+
 t.hideturtle()
 
 t.exitonclick()
